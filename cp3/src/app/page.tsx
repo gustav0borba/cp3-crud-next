9ecb65077d5276081007c7c7ce7d14815ec4c197
Gustavo <gustavo.borba.27@gmail.com>
2024-10-21 20:13:33 -0300

    pequenas alterações para ter uma visualização na home.

--- cp3/src/app/page.tsx
+++ cp3/src/app/page.tsx
@@ -3,7 +3,29 @@
 export default function Home() {
   return (
    <>
+  <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-6">
+      Home
+      </h1>
 
+      <div className=" md:grid-cols-2 gap-4 container flex justify-center">
+        {/* Aqui você pode adicionar componentes, listas ou qualquer outra funcionalidade */}
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-2">Checkpoints</h2>
+          <p>Verificar Notas </p>
+        </div>
+
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-2">Challenge Sprint</h2>
+          <p>Vericar Notas</p>
+        </div>
+
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-2">Global Solutions</h2>
+          <p>Verificar Notas</p>
+        </div>
+      </div>
+    </div>
    </>
   );
 }
--- cp3/src/components/Header/page.tsx
+++ cp3/src/components/Header/page.tsx
@@ -3,6 +3,24 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
+      <header className="bg-gray-600 text-dark shadow-md">
+      <div className="container mx-auto p-4 flex justify-between items-center">
+        {/* Logo ou Título */}
+        <h1 className="text-2xl font-bold">Portifólio Estudante</h1>
+
+        {/* Links de navegação */}
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="hover:text-orange-300">Home</a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-orange-300">Sobre</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
       
     </div>
   )
