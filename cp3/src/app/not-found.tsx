2c484be52351df5d719d5598507b7b3f11d5fc56
arthuramosantos <arthuramossan@gmail.com>
2024-10-22 21:57:56 -0300

    estlizando not-found

--- cp3/src/app/not-found.tsx
+++ cp3/src/app/not-found.tsx
@@ -3,10 +3,19 @@ import notFoundtriste from "@/img/emogi_triste.png";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <Image src= {notFoundtriste} alt='Emoji triste' width={300}/> 
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Not Found...</h1>
-      <p className="text-lg text-gray-600">Esse recurso não está disponível no momento.</p>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-200 overflow-hidden">
+
+      <Image src={notFoundtriste} alt="Emoji triste" width={300} height={200} className="mb-0 mt-[-10%]" />
+      
+      <h1 className="text-4xl font-extrabold text-gray-900 mb-3 mt-[-5%]">404 - Página Não Encontrada</h1>
+      
+      <p className="text-lg text-gray-700 mb-6 mt-5">
+        Parece que o recurso que você está procurando não está disponível no momento.
+      </p>
+      
+      <a href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
+        Voltar à Página Inicial
+      </a>
     </div>
   );
 }
