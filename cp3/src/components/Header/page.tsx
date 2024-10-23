d35a68722343d8bd7666a45802f882bafd764879
arthuramosantos <arthuramossan@gmail.com>
2024-10-22 22:51:48 -0300

    estlização

--- cp3/src/components/Header/page.tsx
+++ cp3/src/components/Header/page.tsx
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
-    <div>
-      <header className="bg-gray-600 text-dark shadow-md">
+    <header className="bg-gray-800 text-white shadow-md">
       <div className="container mx-auto p-4 flex justify-between items-center">
         {/* Logo ou Título */}
-        <h1 className="text-2xl font-bold">Portifólio Estudante</h1>
+        <h1 className="text-3xl font-bold tracking-wide">Portfólio Estudante</h1>
 
         {/* Links de navegação */}
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-8">
             <li>
-              <a href="/" className="hover:text-orange-300">Home</a>
+              <a href="/" className="hover:text-orange-400 transition-colors duration-200 text-lg font-medium">Home</a>
             </li>
             <li>
-              <a href="/about" className="hover:text-orange-300">Sobre</a>
+              <a href="/about" className="hover:text-orange-400 transition-colors duration-200 text-lg font-medium">Sobre</a>
             </li>
           </ul>
         </nav>
       </div>
     </header>
-      
-    </div>
-  )
+  );
 }
