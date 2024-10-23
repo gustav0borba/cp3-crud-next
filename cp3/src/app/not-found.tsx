7af4a6e4f0056a46c5a873726f358b2275d0f73b
arthuramosantos <arthuramossan@gmail.com>
2024-10-22 22:12:53 -0300

    estlização da pagina not found

--- cp3/src/app/not-found.tsx
+++ cp3/src/app/not-found.tsx
@@ -1,21 +1,28 @@
 import Image from 'next/image';
-import notFoundtriste from "@/img/emogi_triste.png";
+import notFoundtriste from "@/img/uma estrada deserta.png"; 
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-200 overflow-hidden">
-
-      <Image src={notFoundtriste} alt="Emoji triste" width={300} height={200} className="mb-0 mt-[-10%]" />
+    <div className="relative flex flex-col justify-center items-center min-h-screen">
+      <Image 
+        src={notFoundtriste} 
+        alt="Estrada deserta" 
+        layout="fill" 
+        objectFit="cover" 
+        className="z-0" 
+      />
       
-      <h1 className="text-4xl font-extrabold text-gray-900 mb-3 mt-[-5%]">404 - Página Não Encontrada</h1>
-      
-      <p className="text-lg text-gray-700 mb-6 mt-5">
-        Parece que o recurso que você está procurando não está disponível no momento.
-      </p>
-      
-      <a href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
-        Voltar à Página Inicial
-      </a>
+      <div className="relative z-10 bg-black bg-opacity-50 p-6 rounded-lg">
+        <h1 className="text-4xl font-extrabold text-white mb-3">404 - Página Não Encontrada</h1>
+        
+        <p className="text-lg text-gray-300 mb-6">
+          Parece que o recurso que você está procurando não está disponível no momento.
+        </p>
+        
+        <a href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
+          Voltar à Página Inicial
+        </a>
+      </div>
     </div>
   );
 }
